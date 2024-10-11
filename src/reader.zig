7abfe09f256677d041812503477ee41672b4238a
Helge Holm <helge.holm@tv2.no>
2024-10-11 15:20:15 +0200

    Adjusted string reader to be used by by a struct reader

--- src/reader.zig
+++ src/reader.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
+const string = @import("string.zig");
 const long = @import("long.zig");
 const main = @import("main.zig");
 
 const ExampleStruct = struct {
+    title: []u8 = &.{},
     count: i16 = 0,
     sum: i64 = 0,
     fn parse(self: *ExampleStruct, buf: []const u8) ![]const u8 {
         var rem = buf;
+        rem = try string.read(&self.title, rem);
         rem = try long.read(i16, &self.count, rem);
         rem = try long.read(i64, &self.sum, rem);
         return rem;
@@ -16,15 +19,20 @@ const ExampleStruct = struct {
 test "parse from avro" {
     var s = ExampleStruct{};
     const buf = &[_]u8{
-        0b10010110,
-        0b11110110,
-        0b00000001,
-        0b10100111,
-        0b10000100,
-        0b00000001,
+        3 << 1, // title(len 3)
+        'H',
+        'A',
+        'Y',
+        0b10010110, // count: 15755
+        0b11110110, // |
+        0b00000001, // |
+        0b10100111, // sum: -8468
+        0b10000100, // |
+        0b00000001, // |
     };
     const rem = try s.parse(buf);
-    try std.testing.expectEqual(s.sum, -8468);
-    try std.testing.expectEqual(s.count, 15755);
-    try std.testing.expectEqual(rem.len, 0);
+    try std.testing.expectEqualStrings("HAY", s.title);
+    try std.testing.expectEqual(-8468, s.sum);
+    try std.testing.expectEqual(15755, s.count);
+    try std.testing.expectEqual(0, rem.len);
 }
