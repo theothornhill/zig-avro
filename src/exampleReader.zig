30d4221787a777f949f3a95cc1a1376a43535e80
Helge Holm <helge.holm@tv2.no>
2024-10-12 17:29:30 +0200

    Transformed to generic reader structs

---
+++ src/exampleReader.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+const string = @import("string.zig");
+const long = @import("long.zig");
+const main = @import("main.zig");
+const reader = @import("reader.zig");
+
+const ExampleStruct = struct {
+    title: reader.String = .{},
+    count: reader.Number(i16) = .{},
+    sum: reader.Number(i64) = .{},
+    pub fn consume(self: *ExampleStruct, buf: []const u8) ![]const u8 {
+        var rem = buf;
+        rem = try self.title.consume(rem);
+        rem = try self.count.consume(rem);
+        rem = try self.sum.consume(rem);
+        return rem;
+    }
+};
+
+test "parse ExampleStruct from avro" {
+    var s = ExampleStruct{};
+    const buf = &[_]u8{
+        3 << 1, // title(len 3)
+        'H',
+        'A',
+        'Y',
+        0b10010110, // count: 15755
+        0b11110110, // |
+        0b00000001, // |
+        0b10100111, // sum: -8468
+        0b10000100, // |
+        0b00000001, // |
+    };
+    const rem = try s.consume(buf);
+    try std.testing.expectEqualStrings("HAY", s.title.value);
+    try std.testing.expectEqual(-8468, s.sum.value);
+    try std.testing.expectEqual(15755, s.count.value);
+    try std.testing.expectEqual(0, rem.len);
+}
+
+test "array of 1 ExampleStruct" {
+    var a = reader.Array(ExampleStruct){};
+    const buf = &[_]u8{
+        1 << 1, // array block length 1
+        2 << 1, // title(len 2)
+        ':',
+        ')',
+        1 << 1, // count: 1
+        2 << 1, // sum: 2
+        0, // array end
+        '?', // stuff beyond the array
+    };
+    const rem = try a.consume(buf);
+    try std.testing.expectEqual(1, rem.len);
+    try std.testing.expectEqual(1, a.len);
+    const s = try a.next() orelse unreachable;
+    try std.testing.expectEqualStrings(":)", s.title.value);
+    try std.testing.expectEqual(1, s.count.value);
+    try std.testing.expectEqual(2, s.sum.value);
+    try std.testing.expectEqual(null, a.next());
+}
--- src/reader.zig
+++ src/reader.zig
@@ -1,191 +1,150 @@
 const std = @import("std");
-const string = @import("string.zig");
 const long = @import("long.zig");
-const main = @import("main.zig");
+const string = @import("string.zig");
 
-const ReadRecordError = long.ReadLongError || string.ReadStringError;
-
-const ReadArrayError = error{
+pub const ReadError = error{
     UninitializedOrSpentIterator,
-} || ReadRecordError;
-
-const ExampleStruct = struct {
-    title: []u8 = &.{},
-    count: i16 = 0,
-    sum: i64 = 0,
-    pub fn consume(self: *ExampleStruct, buf: []const u8) ReadRecordError![]const u8 {
-        var rem = buf;
-        rem = try string.read(&self.title, rem);
-        rem = try long.read(i16, &self.count, rem);
-        rem = try long.read(i64, &self.sum, rem);
-        return rem;
-    }
 };
 
-test "parse ExampleStruct from avro" {
-    var s = ExampleStruct{};
-    const buf = &[_]u8{
-        3 << 1, // title(len 3)
-        'H',
-        'A',
-        'Y',
-        0b10010110, // count: 15755
-        0b11110110, // |
-        0b00000001, // |
-        0b10100111, // sum: -8468
-        0b10000100, // |
-        0b00000001, // |
+pub fn Number(comptime T: type) type {
+    return struct {
+        value: T = 0,
+        pub fn consume(self: *Number(T), buf: []const u8) ![]const u8 {
+            return try long.read(T, &self.value, buf);
+        }
     };
-    const rem = try s.consume(buf);
-    try std.testing.expectEqualStrings("HAY", s.title);
-    try std.testing.expectEqual(-8468, s.sum);
-    try std.testing.expectEqual(15755, s.count);
-    try std.testing.expectEqual(0, rem.len);
 }
 
-const ArrayOfExampleStruct = struct {
-    item: ExampleStruct = .{},
-    len: usize = 0,
-    currentBlockLen: usize = 0,
-    restBuf: []const u8 = &.{},
-    valid: bool = false,
-    /// If there are remaining items in the array, consume the next and return it.
-    /// Returns null if there are no remaining items.
-    ///
-    /// This iterator can only be used once.
-    pub fn next(self: *ArrayOfExampleStruct) ReadArrayError!?*ExampleStruct {
-        if (!self.valid) {
-            return ReadArrayError.UninitializedOrSpentIterator;
-        }
-        if (self.currentBlockLen == 0)
-            self.restBuf = try long.read(usize, &self.currentBlockLen, self.restBuf);
-        if (self.currentBlockLen == 0) {
-            self.valid = false;
-            return null;
-        }
-        self.currentBlockLen -= 1;
-        self.restBuf = try self.item.consume(self.restBuf);
-        return &self.item;
-    }
-    /// Prepares an iterator an returns buffer after end of array.
-    /// Total count of array items stored in self.len.
-    ///
-    /// Arrays contain 0 or more items arranged in 0 or more blocks.
-    /// Each block is a varint `blockItems` describing the number of items in that block.
-    ///
-    /// Large blocks can additionally contain information about the block size in
-    /// bytes allowing us to skip past it faster. This is signalled by having a negative
-    /// `blockItems`, in which we should flip it to positive and read another varint
-    /// describing the `blockBytesLength`.
-    pub fn consume(self: *ArrayOfExampleStruct, buf: []const u8) ReadArrayError![]const u8 {
-        self.restBuf = buf;
-        var blockItems: i64 = 0;
-        var blockBytesLength: usize = 0;
-        var rem: []const u8 = buf;
-        while (true) {
-            rem = try long.read(i64, &blockItems, rem);
-            if (blockItems == 0) {
-                self.valid = true;
-                return rem;
-            } else if (blockItems < 0) {
-                blockItems = -blockItems;
-                rem = try long.read(usize, &blockBytesLength, rem);
-                rem = rem[blockBytesLength..];
-            } else {
-                for (0..@bitCast(blockItems)) |_| {
-                    rem = try self.item.consume(rem);
-                }
-            }
-            self.len += @bitCast(blockItems);
-        }
+pub const String = struct {
+    value: []const u8 = &.{},
+    pub fn consume(self: *String, buf: []const u8) ![]const u8 {
+        return try string.read(&self.value, buf);
     }
 };
 
+pub fn Array(comptime T: type) type {
+    return struct {
+        item: T = .{},
+        len: usize = 0,
+        currentBlockLen: usize = 0,
+        restBuf: []const u8 = &.{},
+        valid: bool = false,
+
+        /// If there are remaining items in the array, consume the next and return it.
+        /// Returns null if there are no remaining items.
+        ///
+        /// This iterator can only be used once.
+        pub fn next(self: *Array(T)) !?*T {
+            if (!self.valid)
+                return ReadError.UninitializedOrSpentIterator;
+            if (self.currentBlockLen == 0)
+                self.restBuf = try long.read(usize, &self.currentBlockLen, self.restBuf);
+            if (self.currentBlockLen == 0) {
+                self.valid = false;
+                return null;
+            }
+            self.currentBlockLen -= 1;
+            self.restBuf = try self.item.consume(self.restBuf);
+            return &self.item;
+        }
+        /// Prepares an iterator an returns buffer after end of array.
+        /// Total count of array items stored in self.len.
+        ///
+        /// Arrays contain 0 or more items arranged in 0 or more blocks.
+        /// Each block is a varint `blockItems` describing the number of items in that block.
+        ///
+        /// Large blocks can additionally contain information about the block size in
+        /// bytes allowing us to skip past it faster. This is signalled by having a negative
+        /// `blockItems`, in which we should flip it to positive and read another varint
+        /// describing the `blockBytesLength`.
+        pub fn consume(self: *Array(T), buf: []const u8) ![]const u8 {
+            self.len = 0;
+            self.restBuf = buf;
+            var blockItems: i64 = 0;
+            var blockBytesLength: usize = 0;
+            var rem: []const u8 = buf;
+            while (true) {
+                rem = try long.read(i64, &blockItems, rem);
+                if (blockItems == 0) {
+                    self.valid = true;
+                    return rem;
+                } else if (blockItems < 0) {
+                    blockItems = -blockItems;
+                    rem = try long.read(usize, &blockBytesLength, rem);
+                    rem = rem[blockBytesLength..];
+                } else {
+                    for (0..@bitCast(blockItems)) |_|
+                        rem = try self.item.consume(rem);
+                }
+                self.len += @bitCast(blockItems);
+            }
+        }
+    };
+}
+
 test "array of 1" {
-    var a = ArrayOfExampleStruct{};
+    var a = Array(Number(i64)){};
     const buf = &[_]u8{
         1 << 1, // array block length 1
-        2 << 1, // title(len 2)
-        ':',
-        ')',
-        1 << 1, // count: 1
-        2 << 1, // sum: 2
+        2 << 1, // number 2
         0, // array end
         '?', // stuff beyond the array
     };
     const rem = try a.consume(buf);
     try std.testing.expectEqual(1, rem.len);
     try std.testing.expectEqual(1, a.len);
-    const s = try a.next() orelse unreachable;
-    try std.testing.expectEqualStrings(":)", s.title);
-    try std.testing.expectEqual(1, s.count);
-    try std.testing.expectEqual(2, s.sum);
+    const i = try a.next() orelse unreachable;
+    try std.testing.expectEqual(2, i.value);
     try std.testing.expectEqual(null, a.next());
 }
 
 test "array of 2" {
-    var a = ArrayOfExampleStruct{};
+    var a = Array(String){};
     const buf = &[_]u8{
         2 << 1, // array block length 2
-        1 << 1, // title(len 2)
+        1 << 1, // string(len 1)
         'A',
-        1 << 1, // count: 1
-        2 << 1, // sum: 2
-        2 << 1, // title(len 2)
+        2 << 1, // string(len 2)
         'B',
         'C',
-        3 << 1, // count: 3
-        4 << 1, // sum: 4
         0, // array end
     };
     const rem = try a.consume(buf);
     try std.testing.expectEqual(0, rem.len);
     try std.testing.expectEqual(2, a.len);
-    var s = try a.next() orelse unreachable;
-    try std.testing.expectEqualStrings("A", s.title);
-    try std.testing.expectEqual(1, s.count);
-    try std.testing.expectEqual(2, s.sum);
-    s = try a.next() orelse unreachable;
-    try std.testing.expectEqualStrings("BC", s.title);
-    try std.testing.expectEqual(3, s.count);
-    try std.testing.expectEqual(4, s.sum);
+    var i = try a.next() orelse unreachable;
+    try std.testing.expectEqualStrings("A", i.value);
+    i = try a.next() orelse unreachable;
+    try std.testing.expectEqualStrings("BC", i.value);
     try std.testing.expectEqual(null, a.next());
 }
 
 test "array of 2 in 2 blocks" {
-    var a = ArrayOfExampleStruct{};
+    var a = Array(String){};
     const buf = &[_]u8{
         1 << 1, // array block#1 length 1
         1 << 1, // title(len 2)
         'A',
-        1 << 1, // count: 1
-        2 << 1, // sum: 2
         1 << 1, // array block#2 length 1
         2 << 1, // title(len 2)
         'B',
         'C',
-        3 << 1, // count: 3
-        4 << 1, // sum: 4
         0, // array end
     };
     const rem = try a.consume(buf);
     try std.testing.expectEqual(0, rem.len);
     try std.testing.expectEqual(2, a.len);
-    var s = try a.next() orelse unreachable;
-    try std.testing.expectEqualStrings("A", s.title);
-    try std.testing.expectEqual(1, s.count);
-    try std.testing.expectEqual(2, s.sum);
-    s = try a.next() orelse unreachable;
-    try std.testing.expectEqualStrings("BC", s.title);
-    try std.testing.expectEqual(3, s.count);
-    try std.testing.expectEqual(4, s.sum);
+    var i = try a.next() orelse unreachable;
+    try std.testing.expectEqualStrings("A", i.value);
+    i = try a.next() orelse unreachable;
+    try std.testing.expectEqualStrings("BC", i.value);
     try std.testing.expectEqual(null, a.next());
 }
 
 // Array blocks are purposefully given invalid data, as the array consume()
 // should skip over them. They will only cause trouble once iterated over.
 test "array with marked-length blocks" {
-    var a = ArrayOfExampleStruct{};
+    var a = Array(String){};
     const buf = &[_]u8{
         (3 << 1) - 1, // block#1 size -3
         12 << 1, // block#1 byte length 12
@@ -200,7 +159,7 @@ test "array with marked-length blocks" {
 }
 
 test "array of 0" {
-    var a = ArrayOfExampleStruct{};
+    var a = Array(Number(u64)){};
     const buf = &[_]u8{
         0, // array end
     };
@@ -210,13 +169,52 @@ test "array of 0" {
 }
 
 test "incorrect usage" {
-    var a = ArrayOfExampleStruct{};
+    var a = Array(Number(i32)){};
     const buf = &[_]u8{
         0, // array end
     };
-    try std.testing.expectError(ReadArrayError.UninitializedOrSpentIterator, a.next());
+    try std.testing.expectError(ReadError.UninitializedOrSpentIterator, a.next());
     const rem = try a.consume(buf);
     try std.testing.expectEqual(0, rem.len);
     try std.testing.expectEqual(null, a.next());
-    try std.testing.expectError(ReadArrayError.UninitializedOrSpentIterator, a.next());
+    try std.testing.expectError(ReadError.UninitializedOrSpentIterator, a.next());
+}
+
+// +-+-+
+// |1|2|
+// +-+-+
+// |3|4|
+// +-+-+
+test "2d array" {
+    var a = Array(Array(Number(i32))){};
+    const buf = &[_]u8{
+        2 << 1, // 2 rows
+        2 << 1, // 1st row: 2 columns
+        1 << 1, // R1C1=1
+        2 << 1, // R1C2=2
+        0, // cols end
+        2 << 1, // 2nd row: 2 columns
+        3 << 1, // R2C1=3
+        4 << 1, // R2C2=4
+        0, // cols end
+        0, // rows end
+    };
+    const rem = try a.consume(buf);
+    try std.testing.expectEqual(0, rem.len);
+    try std.testing.expectEqual(2, a.len);
+    const row1 = try a.next() orelse unreachable;
+    try std.testing.expectEqual(2, row1.len);
+    var cell = try row1.next() orelse unreachable;
+    try std.testing.expectEqual(1, cell.value);
+    cell = try row1.next() orelse unreachable;
+    try std.testing.expectEqual(2, cell.value);
+    try std.testing.expectEqual(null, row1.next());
+    const row2 = try a.next() orelse unreachable;
+    try std.testing.expectEqual(2, row2.len);
+    cell = try row2.next() orelse unreachable;
+    try std.testing.expectEqual(3, cell.value);
+    cell = try row2.next() orelse unreachable;
+    try std.testing.expectEqual(4, cell.value);
+    try std.testing.expectEqual(null, row2.next());
+    try std.testing.expectEqual(null, a.next());
 }
