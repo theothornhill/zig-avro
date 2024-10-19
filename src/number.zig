3aa5dcc254d5c04c646d223b5d34a00dcde08f0a
Theodor Thornhill <theodor.thornhill@tv2.no>
2024-10-19 11:50:35 +0200

    WIP number refactor

--- src/number.zig
+++ src/number.zig
@@ -10,7 +10,55 @@ pub const WriteLongError = error{
     Overflow,
 };
 
-pub fn read(comptime T: type, dst: *T, buf: []const u8) ![]const u8 {
+pub fn readUint(dst: *u32, buf: []const u8) ![]const u8 {
+    return readNumber(u32, dst, buf);
+}
+
+pub fn readInt(dst: *i32, buf: []const u8) ![]const u8 {
+    return readNumber(i32, dst, buf);
+}
+
+pub fn readUlong(dst: *u64, buf: []const u8) ![]const u8 {
+    return readNumber(u64, dst, buf);
+}
+
+pub fn readLong(dst: *i64, buf: []const u8) ![]const u8 {
+    return readNumber(i64, dst, buf);
+}
+
+pub fn writeUint(value: u32, buf: []u8) !void {
+    return writeNumber(u32, value, buf);
+}
+
+pub fn writeInt(value: i32, buf: []u8) !void {
+    return writeNumber(i32, value, buf);
+}
+
+pub fn writeUlong(value: u64, buf: []u8) !void {
+    return writeNumber(u64, value, buf);
+}
+
+pub fn writeLong(value: i64, buf: []u8) !void {
+    return writeNumber(i64, value, buf);
+}
+
+pub fn readFloat(dst: *f32, buf: []const u8) ![]const u8 {
+    return readFloatingPointNumber(f32, dst, buf);
+}
+
+pub fn readDouble(dst: *f64, buf: []const u8) ![]const u8 {
+    return readFloatingPointNumber(f64, dst, buf);
+}
+
+pub fn writeFloat(value: f32, buf: []u8) !void {
+    return writeFloatingPointNumber(f32, value, buf);
+}
+
+pub fn writeDouble(value: f64, buf: []u8) !void {
+    return writeFloatingPointNumber(f64, value, buf);
+}
+
+fn readNumber(comptime T: type, dst: *T, buf: []const u8) ![]const u8 {
     const U: type = switch (T) {
         i32 => u32,
         i64 => u64,
@@ -27,7 +75,7 @@ pub fn read(comptime T: type, dst: *T, buf: []const u8) ![]const u8 {
     return buf[try stream.getPos()..];
 }
 
-pub fn write(comptime T: type, value: T, buf: []u8) !void {
+fn writeNumber(comptime T: type, value: T, buf: []u8) !void {
     const U: type = switch (T) {
         i32 => u32,
         i64 => u64,
@@ -56,6 +104,89 @@ inline fn zigZagDecode(comptime T: type, n: T) T {
     return if (n & 1 == 1) ~(n >> 1) else (n >> 1);
 }
 
+pub const ReadFloatingPointError = error{
+    Overflow,
+    InvalidEOF,
+};
+
+pub const WriteFloatingPointError = error{
+    Overflow,
+};
+
+fn readFloatingPointNumber(comptime T: type, dst: *T, buf: []const u8) ![]const u8 {
+    const U: type = switch (T) {
+        f32 => u32,
+        f64 => u64,
+        else => @compileError("unsupported type: " ++ @typeName(T)),
+    };
+
+    if (buf.len < @sizeOf(U)) {
+        return error.InvalidEOF;
+    }
+
+    var stream = std.io.fixedBufferStream(buf);
+    dst.* = @bitCast(try stream.reader().readInt(U, .big));
+
+    return buf[@sizeOf(U)..];
+}
+
+fn writeFloatingPointNumber(comptime T: type, value: T, buf: []u8) !void {
+    const U: type = switch (T) {
+        f32 => u32,
+        f64 => u64,
+        else => @compileError("unsupported type: " ++ @typeName(T)),
+    };
+    var stream = std.io.fixedBufferStream(buf);
+    try stream.writer().writeInt(U, @bitCast(value), .big);
+}
+
+test "read float and double" {
+    var test_f32: f32 = undefined;
+
+    const rem_f32 = try readFloat(&test_f32, &[_]u8{
+        0x40, 0x49, 0x0F, 0xD8,
+    });
+    try std.testing.expectApproxEqRel(3.141592, test_f32, std.math.floatEps(f32));
+    try std.testing.expectEqual(0, rem_f32.len);
+
+    var test_f64: f64 = undefined;
+
+    const rem_f64 = try readDouble(&test_f64, &[_]u8{
+        0x40, 0x09, 0x21, 0xFB, 0x54, 0x44, 0x2D, 0x18,
+    });
+    try std.testing.expectApproxEqRel(
+        3.141592653589793115997963468544185161590576171875,
+        test_f64,
+        std.math.floatEps(f64),
+    );
+    try std.testing.expectEqual(0, rem_f64.len);
+}
+
+test "write float and double" {
+    var res = &[_]u8{
+        0x40, 0x49, 0x0F, 0xD8,
+    };
+
+    var buf: [4]u8 = undefined;
+    try writeFloat(3.141592, &buf);
+    try std.testing.expectEqualSlices(u8, res, &buf);
+
+    res = &[_]u8{
+        0xC0, 0x49, 0x0F, 0xD8,
+    };
+    buf = undefined;
+    try writeFloat(-3.141592, &buf);
+    try std.testing.expectEqualSlices(u8, res, &buf);
+
+    const res2 = &[_]u8{
+        0x40, 0x09, 0x21, 0xFB, 0x54, 0x44, 0x2D, 0x18,
+    };
+
+    var buf2: [8]u8 = undefined;
+    try writeDouble(3.141592653589793115997963468544185161590576171875, &buf2);
+    try std.testing.expectEqualSlices(u8, res2, &buf2);
+}
+
 test "zig zag fuzz" {
     const testFuncs = struct {
         fn test64(input: []const u8) !void {
@@ -74,18 +205,18 @@ test "zig zag fuzz" {
     try std.testing.fuzz(testFuncs.test32, .{});
 }
 
-test read {
+test "read int and long" {
     var test_i32: i32 = 123;
     var test_u64: u64 = 123;
 
-    try std.testing.expectError(error.EndOfStream, read(u64, &test_u64, &[_]u8{
+    try std.testing.expectError(error.EndOfStream, readUlong(&test_u64, &[_]u8{
         0b10010110,
         0b10010110,
         0b10010110,
         0b10010110,
     }));
 
-    const rem_i32 = try read(i32, &test_i32, &[_]u8{
+    const rem_i32 = try readInt(&test_i32, &[_]u8{
         0b10010110,
         0b1,
         0b1,
@@ -93,7 +224,7 @@ test read {
     try std.testing.expectEqual(rem_i32.len, 1);
     try std.testing.expectEqual(75, test_i32);
 
-    const rem_u64 = try read(u64, &test_u64, &[_]u8{
+    const rem_u64 = try readUlong(&test_u64, &[_]u8{
         0b10010110,
         0b1,
         0b1,
@@ -102,11 +233,11 @@ test read {
     try std.testing.expectEqual(75, test_u64);
 }
 
-test write {
+test "write int and long" {
     const res = &[_]u8{ 0xAC, 0x02 };
 
     var buf: [2]u8 = undefined;
-    try write(u64, 150, &buf);
+    try writeUint(150, &buf);
     try std.testing.expectEqualSlices(u8, res, &buf);
 
     const negativeTwo = &[_]u8{
@@ -114,14 +245,14 @@ test write {
     };
 
     var negBuf: [1]u8 = undefined;
-    try write(u64, 0xfffffffffffffffe, &negBuf);
+    try writeUlong(0xfffffffffffffffe, &negBuf);
     try std.testing.expectEqualSlices(u8, negativeTwo, &negBuf);
 
     var negBuf2: [0]u8 = undefined;
-    try std.testing.expectError(error.NoSpaceLeft, write(u64, 0xfffffffffffffffe, &negBuf2));
+    try std.testing.expectError(error.NoSpaceLeft, writeUlong(0xfffffffffffffffe, &negBuf2));
 
     var buf3: [0]u8 = undefined;
-    try std.testing.expectError(error.NoSpaceLeft, write(u64, 1, &buf3));
+    try std.testing.expectError(error.NoSpaceLeft, writeUlong(1, &buf3));
 }
 
 test zigZagEncode {
