2b5fae9d3c2f19a7747336762086246c663f1703
Helge Holm <helge.holm@tv2.no>
2024-10-29 08:59:10 +0100

    Add enum writer

--- src/writer.zig
+++ src/writer.zig
@@ -17,6 +17,7 @@ pub fn write(comptime T: type, v: *T, buf: []u8) ![]const u8 {
                 .@"struct" => {
                     return try writeRecord(T, v, buf);
                 },
+                .@"enum" => return try writeEnum(T, v.*, buf),
                 else => {},
             }
             @compileError("unsupported field type " ++ @typeName(T));
@@ -24,11 +25,45 @@ pub fn write(comptime T: type, v: *T, buf: []u8) ![]const u8 {
     }
 }
 
+fn writeEnum(comptime E: type, e: E, buf: []u8) ![]const u8 {
+    return try number.writeInt(@as(i32, @intFromEnum(e)), buf);
+}
+
 fn writeRecord(comptime R: type, r: *R, buf: []u8) ![]const u8 {
     var written: usize = 0;
     inline for (@typeInfo(R).@"struct".fields) |field|
         written += (try write(field.type, &@field(r, field.name), buf[written..])).len;
     return buf[0..written];
+}
+
+test "write record with enum" {
+    const Language = enum {
+        zig,
+        go,
+        rust,
+        java,
+    };
+    const Record = struct {
+        cool: Language,
+        cooler: Language,
+    };
+    var writeBuffer: [100]u8 = undefined;
+    var buf: []u8 = writeBuffer[0..100];
+    var r1: Record = .{
+        .cool = .go,
+        .cooler = .rust,
+    };
+    const msg1 = try write(Record, &r1, buf);
+    var r2: Record = .{
+        .cool = .rust,
+        .cooler = .zig,
+    };
+    _ = try write(Record, &r2, buf[msg1.len..]);
+    var ro: Record = undefined;
+    const rem = try reader.read(Record, &ro, buf);
+    try std.testing.expectEqual(r1, ro);
+    _ = try reader.read(Record, &ro, rem);
+    try std.testing.expectEqual(r2, ro);
 }
 
 test "write record of primitives" {
