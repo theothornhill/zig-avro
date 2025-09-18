4a7d16553a27adda4b9cd875941ba8afeaa75cec
Theodor Thornhill <theo@thornhill.no>
2025-09-18 12:52:33 +0200

    Add another root_module for 0.15 compatibility

--- build.zig
+++ build.zig
@@ -12,9 +12,12 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .name = "tests",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
